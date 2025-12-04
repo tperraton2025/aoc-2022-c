04fff07e84bf0807eeb3857762b73002df4665dd
TankManDu70 <thomas.perraton@gmail.com>
2025-12-04 16:22:19 +0100

    fix on link list consecutive nodes

--- lib/inc/aoc_linked_list.h
+++ lib/inc/aoc_linked_list.h
@@ -5,8 +5,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define LL_MAX_LEN (65535)
-#define NODE_CAST(_p) ((struct dll_node *)_p)
+#define LL_MAX_LEN_LUI (65535)
+#define LL_MAX_LEN_STR "65535"
+
+#define NODE_CAST(_p) ((dll_node_h)_p)
 #define MAX_STR_DLL_LEN (1024)
 
 struct dll_head
@@ -22,37 +24,43 @@ typedef struct dll_head dll_head_t;
 
 struct dll_node
 {
-    struct dll_node *_prev;
-    struct dll_node *_next;
+    struct dll_node * _prev;
+    struct dll_node * _next;
     void (*free)(void *_data);
 };
 
+typedef struct dll_node *dll_node_h;
 typedef struct string_dll_node
 {
     struct dll_node _node;
     char *_str;
 } string_dll_node_t;
 
-typedef struct dll_node *(dll_compare)(struct dll_node * _a, struct dll_node *_b);
+typedef dll_node_h(dll_compare)(dll_node_h _a, dll_node_h _b);
 
 typedef struct string_dll_node *string_dll_node_h;
 
 void dll_head_init(dll_head_h head);
-void dll_sort(dll_head_h head, dll_compare(*comp));
+int dll_sort(dll_head_h head, dll_compare(*comp));
 void dll_free_all(dll_head_h head, void (*_caller)(void *_data));
 
-int dll_node_append(dll_head_h head, struct dll_node *_new);
-int dll_find_node(dll_head_h head, struct dll_node *_a);
-int dll_node_sorted_insert(dll_head_h head, struct dll_node *_new, dll_compare sort);
+void dll_foreach(dll_head_h head, void *arg, void(func)(void *arga, void *argb));
 
-void dll_node_permut(dll_head_h head, struct dll_node *_a, struct dll_node *_b);
-void dll_node_insert(dll_head_h head, struct dll_node *_a, struct dll_node *_b);
-void dll_node_disconnect(dll_head_h head, struct dll_node *_a);
+int dll_node_append(dll_head_h head, dll_node_h _new);
+int dll_find_node(dll_head_h head, dll_node_h _a);
+int dll_node_insert(dll_head_h head, dll_node_h _a, dll_node_h _b);
+int dll_node_sorted_insert(dll_head_h head, dll_node_h _new, dll_compare sort);
+int dll_trim_nodes(dll_head_h head, bool (*ifeq)(void *_a), void(free)(void *arg));
+
+dll_head_h dll_clone_trimmed(dll_head_h head, size_t nodesize, void *_prop, bool (*ifeq)(void *arg, void *prop));
+void dll_node_swap(dll_head_h head, dll_node_h _a, dll_node_h _b);
+void dll_node_disconnect(dll_head_h head, dll_node_h _a);
 
 size_t dll_size(dll_head_h head);
 size_t dll_count_nodes_by_property(dll_head_h head, void *_prop, bool (*equal)(void *_a, void *_b));
+dll_head_h dll_clone_sorted(dll_head_h head, dll_compare(*comp), size_t nodesize);
 
-struct dll_node *dll_find_node_by_property(dll_head_h head, void *_prop, bool (*equal)(void *_a, void *_b));
+dll_node_h dll_find_node_by_property(dll_head_h head, void *_prop, bool (*equal)(void *_a, void *_b));
 
 #define DLL_DECLARE(_type) \
     struct dll_node _node;
@@ -74,12 +82,12 @@ struct dll_node *dll_find_node_by_property(dll_head_h head, void *_prop, bool (*
         if (_it)
 
 #define LL_FOREACH_P(_it, _head_h)                             \
-    struct dll_node *_it;                                      \
+    dll_node_h _it;                                            \
     for (_it = _head_h->_first; _it != NULL; _it = _it->_next) \
         if (_it)
 
 #define LL_FOREACH(_it, _head)                              \
-    struct dll_node *_it;                                   \
+    dll_node_h _it;                                         \
     for (_it = _head._first; _it != NULL; _it = _it->_next) \
         if (_it)
 
@@ -92,14 +100,14 @@ struct dll_node *dll_find_node_by_property(dll_head_h head, void *_prop, bool (*
         if (_it)
 
 #define LL_FOREACH_LAST(_it, _blk) \
-    struct dll_node *_it;          \
+    dll_node_h _it;                \
     LL_FOREACH_LAST_EXT(_it, _blk)
 
 void string_dll_free(void *string);
 
 string_dll_node_h string_dll(const char *const name);
 
-struct dll_node *string_dll_compare(struct dll_node *_a, struct dll_node *_b);
+dll_node_h string_dll_compare(dll_node_h _a, dll_node_h _b);
 
 int string_dll_rename(string_dll_node_h node, const char *const name);
 
--- lib/tests/test_linked_lists.c
+++ lib/tests/test_linked_lists.c
@@ -1,5 +1,6 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
+#include <stdlib.h>
 
 #include <assert.h>
 #include <stdlib.h>
@@ -7,15 +8,35 @@
 #include <aoc_helpers.h>
 #include <aoc_linked_list.h>
 #define TEST_STRING_MAX_LEN (512)
-#define TS_CAST(_p) ((_string_test_t *)_p)
+#define TS_CAST(_p) ((teststr_t *)_p)
 
 typedef struct
 {
     struct dll_node _node;
     char *_str;
-} _string_test_t;
+} teststr_t;
 
-static _string_test_t *_string_ll_test(const char *const _str);
+typedef teststr_t *teststr_h;
+
+#define ASSERT_LL_INTEGRITY(_ll, ...)                                       \
+    {                                                                       \
+        teststr_h _exp[] = {__VA_ARGS__};                                   \
+        CU_ASSERT(dll_size(&_ll) == ARRAY_DIM(_exp));                       \
+        CU_ASSERT(TS_CAST(_ll._first->_prev) == NULL);                      \
+        CU_ASSERT(TS_CAST(_ll._first) == _exp[0]);                          \
+        CU_ASSERT(TS_CAST(_ll._last) == _exp[ARRAY_DIM(_exp) - 1]);         \
+        dll_node_h _node = _ll._first;                                \
+        ARR_FOREACH(_ii, _exp)                                              \
+        {                                                                   \
+            CU_ASSERT(TS_CAST(_node) == _exp[_ii]);                         \
+            _node = _node->_next;                                           \
+        }                                                                   \
+        CU_ASSERT(TS_CAST(_exp[ARRAY_DIM(_exp) - 1])->_node._next == NULL); \
+    }
+
+static teststr_h _string_ll_test(const char *const _str);
+static dll_node_h _strinsortbyname(dll_node_h arga, dll_node_h argb);
+static void print(void *arga, void *argb);
 static void free_test_string(void *_node);
 static void ll_test_suite();
 static bool _string_node_equal(void *_a, void *_b);
@@ -40,81 +61,51 @@ void ll_test_suite()
     struct dll_head _ll = {0};
     dll_head_init(&_ll);
 
-    _string_test_t *_str1 = _string_ll_test("foo 1");
-    _string_test_t *_str2 = _string_ll_test("foo 2");
-    _string_test_t *_str3 = _string_ll_test("foo 3");
-    _string_test_t *_str4 = _string_ll_test("foo 4");
+    CU_ASSERT_EQUAL(atoi(LL_MAX_LEN_STR), LL_MAX_LEN_LUI);
 
-    _string_test_t *_strings[] = {_str1,
-                                  _str2,
-                                  _str3,
-                                  _str4};
+    teststr_h _str1 = _string_ll_test("foo 1");
+    teststr_h _str2 = _string_ll_test("foo 2");
+    teststr_h _str3 = _string_ll_test("foo 3");
+    teststr_h _str4 = _string_ll_test("foo 4");
+
+    teststr_h _strings[] = {_str1,
+                            _str2,
+                            _str3,
+                            _str4};
 
     for (size_t _ii = 0; _ii < sizeof(_strings) / sizeof(_strings[0]); _ii++)
     {
-        dll_node_append(&_ll, (struct dll_node *)_strings[_ii]);
+        dll_node_append(&_ll, (dll_node_h )_strings[_ii]);
     }
-
-    CU_ASSERT(dll_size(&_ll) == 4);
-    CU_ASSERT(TS_CAST(_ll._first->_prev) == NULL);
-
-    CU_ASSERT(TS_CAST(_ll._last) == _str4);
-    CU_ASSERT(TS_CAST(_ll._first) == _str1);
-    CU_ASSERT(TS_CAST(_ll._first->_next) == _str2);
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next) == _str3);
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next->_next) == _str4);
-
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next->_next->_next) == NULL);
+    ASSERT_LL_INTEGRITY(_ll, _str1, _str2, _str3, _str4);
 
     dll_node_disconnect(&_ll, _ll._first->_next);
-
-    CU_ASSERT(dll_size(&_ll) == 3);
-    CU_ASSERT(TS_CAST(_ll._first->_prev) == NULL);
-
-    CU_ASSERT(TS_CAST(_ll._last) == _str4);
-    CU_ASSERT(TS_CAST(_ll._first) == _str1);
-    CU_ASSERT(TS_CAST(_ll._first->_next) == _str3);
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next) == _str4);
+    ASSERT_LL_INTEGRITY(_ll, _str1, _str3, _str4);
     CU_ASSERT(ENOENT == dll_find_node(&_ll, &_str2->_node))
 
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next->_next) == NULL);
+    dll_node_swap(&_ll, _ll._first, _ll._last);
+    ASSERT_LL_INTEGRITY(_ll, _str4, _str3, _str1);
 
-    dll_node_permut(&_ll, _ll._first, _ll._last);
-
-    CU_ASSERT(dll_size(&_ll) == 3);
-    CU_ASSERT(TS_CAST(_ll._first->_prev) == NULL);
-
-    CU_ASSERT(TS_CAST(_ll._last) == _str1);
-    CU_ASSERT(TS_CAST(_ll._first) == _str4);
-    CU_ASSERT(TS_CAST(_ll._first->_next) == _str3);
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next) == _str1);
-
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next->_next) == NULL);
-    CU_ASSERT(_ll._first->_next->_next == _ll._last);
+    dll_node_swap(&_ll, &_str4->_node, &_str3->_node);
+    ASSERT_LL_INTEGRITY(_ll, _str3, _str4, _str1);
 
     dll_node_insert(&_ll, &_str2->_node, _ll._last);
 
-    CU_ASSERT(dll_size(&_ll) == 4);
-    CU_ASSERT(TS_CAST(_ll._first->_prev) == NULL);
+    ASSERT_LL_INTEGRITY(_ll, _str3, _str4, _str2, _str1);
 
-    CU_ASSERT(TS_CAST(_ll._first) == _str4);
-    CU_ASSERT(TS_CAST(_ll._first->_next) == _str3);
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next) == _str2);
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next->_next) == _str1);
-
-    CU_ASSERT(TS_CAST(_ll._first->_next->_next->_next->_next) == NULL);
-    CU_ASSERT(_ll._first->_next->_next->_next == _ll._last);
+    dll_sort(&_ll, _strinsortbyname);
+    ASSERT_LL_INTEGRITY(_ll, _str1, _str2, _str3, _str4);
 
     CU_ASSERT(1 == dll_count_nodes_by_property(&_ll, _str4->_str, _string_node_equal));
     CU_ASSERT(_str4 == TS_CAST(dll_find_node_by_property(&_ll, _str4->_str, _string_node_equal)));
 
     dll_free_all(&_ll, free_test_string);
-    aoc_ans("%s", "ll_test_suite: all tests passed");
+    aoc_info("%s", "ll_test_suite: all tests were ran");
 }
 
-static _string_test_t *_string_ll_test(const char *const _str)
+static teststr_h _string_ll_test(const char *const _str)
 {
-    _string_test_t *_nstr = malloc(sizeof(_string_test_t));
+    teststr_h _nstr = malloc(sizeof(teststr_t));
     assert(_nstr && "_string_ll_test malloc failed");
 
     _nstr->_str = malloc(strnlen(_str, TEST_STRING_MAX_LEN + 1) + 1);
@@ -124,9 +115,15 @@ static _string_test_t *_string_ll_test(const char *const _str)
     return _nstr;
 }
 
+static void print(void *arga, void *argb)
+{
+    teststr_h _nstr = (teststr_h)arga;
+    printf("%s", _nstr->_str);
+}
+
 static void free_test_string(void *_node)
 {
-    _string_test_t *_nstr = (_string_test_t *)_node;
+    teststr_h _nstr = (teststr_h)_node;
     FREE(_nstr->_str);
     _nstr->_str = NULL;
     FREE(_node);
@@ -136,4 +133,11 @@ static void free_test_string(void *_node)
 static bool _string_node_equal(void *_a, void *_b)
 {
     return 0 == strncmp((const char *)TS_CAST(_a)->_str, (const char *)_b, TEST_STRING_MAX_LEN);
+}
+
+static dll_node_h _strinsortbyname(dll_node_h arga, dll_node_h argb)
+{
+    teststr_h _jolta = (teststr_h)arga;
+    teststr_h _joltb = (teststr_h)argb;
+    return strcmp(_jolta->_str, _joltb->_str) >= 0 ? arga : argb;
 }
